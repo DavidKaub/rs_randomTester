860816ffd0cac231bb141a87057b0c5d2503b680
thefeinkoster <s4dakaub@uni-trier.de>
2019-10-25 18:51:58 +0200

    Edited JZombiesBuilder to test seed / random generator
    - Set Seed to static value (1)
    - Added JZombiesBuilder to context in order to enable scheduling
    - Created ScheduledMethod step() which generates some random Numbers and
    stops the run after 10 ticks. It then prints the addition of all x and y
    coordinates as well as the multiplication of some random numbers. This
    method is quite random but should enable tracking of the changes in the
    position of the Zombies as well as as the current random numbers

--- JZombies_Demo/src/jzombies/JZombiesBuilder.java
+++ JZombies_Demo/src/jzombies/JZombiesBuilder.java
@@ -1,5 +1,7 @@
 package jzombies;
 
+import java.util.Iterator;
+
 import repast.simphony.context.Context;
 import repast.simphony.context.DefaultContext;
 import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
@@ -9,6 +11,8 @@ import repast.simphony.context.space.grid.GridFactory;
 import repast.simphony.context.space.grid.GridFactoryFinder;
 import repast.simphony.dataLoader.ContextBuilder;
 import repast.simphony.engine.environment.RunEnvironment;
+import repast.simphony.engine.schedule.ScheduleParameters;
+import repast.simphony.engine.schedule.ScheduledMethod;
 import repast.simphony.parameter.Parameters;
 import repast.simphony.random.RandomHelper;
 import repast.simphony.space.continuous.ContinuousSpace;
@@ -18,8 +22,43 @@ import repast.simphony.space.grid.Grid;
 import repast.simphony.space.grid.GridBuilderParameters;
 import repast.simphony.space.grid.SimpleGridAdder;
 import repast.simphony.space.grid.WrapAroundBorders;
+import repast.simphony.util.ContextUtils;
 
 public class JZombiesBuilder implements ContextBuilder<Object> {
+	
+	
+	private Grid<Object> grid;
+
+	/*
+	 * This method creates some random numbers and terminates the simulation after
+	 * 10 ticks. Then the sum of all coordinates of all Zombies as well as a
+	 * multiplication of some random numbers are exited
+	 */
+	@ScheduledMethod(start = 1, interval = 1, priority = ScheduleParameters.LAST_PRIORITY)
+	public void step() {
+		double test = 0;
+		for (int i = 0; i < RandomHelper.nextIntFromTo(1, 10); i++) {
+			test = RandomHelper.nextDouble();
+		}
+
+		if (RunEnvironment.getInstance().getCurrentSchedule().getTickCount() > 10) {
+			Iterator it = ContextUtils.getContext(this).getObjects(Zombie.class).iterator();
+			int x = 0;
+			int y = 0;
+			while (it.hasNext()) {
+				Zombie z = (Zombie) it.next();
+
+				x += grid.getLocation(z).getX();
+				y += grid.getLocation(z).getY();
+			}
+
+			System.out.println(
+					"x = " + x + " y = " + y + " test = " + test * RandomHelper.nextDoubleFromTo(1.0d, 100.0d));
+			RunEnvironment.getInstance().endRun();
+		}
+	}
+	
+	
 
 	/*
 	 * (non-Javadoc)
@@ -32,6 +71,13 @@ public class JZombiesBuilder implements ContextBuilder<Object> {
 	public Context build(Context<Object> context) {
 		context.setId("jzombies");
 
+		
+		//here I set the random seed to a static value to ensure constant input
+		RandomHelper.setSeed(1);
+
+		//here I add the JZombiesBuilder to the context to enable the call of the ScheduledMethod
+		context.add(this);
+		
 		NetworkBuilder<Object> netBuilder = new NetworkBuilder<Object>(
 				"infection network", context, true);
 		netBuilder.buildNetwork();
